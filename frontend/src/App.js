aef37675b59b5ea0cd5c785f379807845ae52488
Karim Abboud <kaa75@mail.aub.edu>
2024-05-23 21:46:22 +0300

    there is a login screen now with some basic functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Signin from './signin_up.js';
 
-const DEMO_ACCOUNTS = [{ email : "xyz01@mail.aub.edu", password: 123}];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			{/* <div>
+				Sign in/Sign Up
+			</div> */}
+			{Signin()}
+		</div>
+	);
 }
 
 export default App;
