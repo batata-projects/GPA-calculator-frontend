72bf6eccf0fa7bbfd7223c0f534a093ea920ba2d
Karim Abboud <kaa75@mail.aub.edu>
2024-05-24 17:07:14 +0300

    added one dependency added 3 new pages added css for each page renamed signin_up.js to signin.js created routes for each page

---
+++ frontend/src/pages/welcome/welcome.js
@@ -0,0 +1,19 @@
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome to the GPA Calculator!</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/signin">Sign In</a>
+                    </li>
+                    <li>
+                        <a href="/signup">Sign Up</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Welcome;
