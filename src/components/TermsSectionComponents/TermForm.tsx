b6bb2015bc4e85670ca170ec379d4cfa3a04f76b
Fadibagh <fadi2002.ba@gmail.com>
2024-07-12 06:39:45 +0300

    moved letter grades and numeric grades outside the function in termform

--- src/components/TermsSectionComponents/TermForm.tsx
+++ src/components/TermsSectionComponents/TermForm.tsx
@@ -17,6 +17,36 @@ interface TermFormProps {
   refreshToken: string | number | boolean;
 }
 
+const letterGrades = [
+  "A+",
+  "A",
+  "A-",
+  "B+",
+  "B",
+  "B-",
+  "C+",
+  "C",
+  "C-",
+  "D+",
+  "D",
+  "F",
+];
+
+const gradeValues: { [key: string]: number } = {
+  "A+": 4.3,
+  A: 4.0,
+  "A-": 3.7,
+  "B+": 3.3,
+  B: 3.0,
+  "B-": 2.7,
+  "C+": 2.3,
+  C: 2.0,
+  "C-": 1.7,
+  "D+": 1.3,
+  D: 1.0,
+  F: 0.0,
+};
+
 const TermForm: React.FC<TermFormProps> = ({
   onClose,
   userId,
@@ -36,36 +66,6 @@ const TermForm: React.FC<TermFormProps> = ({
     },
   ]);
   const [error, setError] = useState("");
-  const letterGrades = [
-    "A+",
-    "A",
-    "A-",
-    "B+",
-    "B",
-    "B-",
-    "C+",
-    "C",
-    "C-",
-    "D+",
-    "D",
-    "F",
-  ];
-
-  const gradeValues: { [key: string]: number } = {
-    "A+": 4.3,
-    A: 4.0,
-    "A-": 3.7,
-    "B+": 3.3,
-    B: 3.0,
-    "B-": 2.7,
-    "C+": 2.3,
-    C: 2.0,
-    "C-": 1.7,
-    "D+": 1.3,
-    D: 1.0,
-    F: 0.0,
-  };
-
   const formContainerRef = useRef<HTMLDivElement>(null);
   const semesters = ["Fall", "Winter", "Spring", "Summer"];
 
