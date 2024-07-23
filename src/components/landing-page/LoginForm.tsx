ff76fe18723438bfc357f30cced852119dacaea9
Fadibagh <fadi2002.ba@gmail.com>
2024-07-23 10:07:57 +0300

    added icons to landing page forms

--- src/components/landing-page/LoginForm.tsx
+++ src/components/landing-page/LoginForm.tsx
@@ -19,23 +19,59 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
       <div className=" text-[26px] text-[#333] font-bold">Hello Again!</div>
       <div className=" text-lg text-[#333] mb-[40px]">Welcome Back</div>
       <form onSubmit={handleSubmit} className=" text-md">
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          id="email"
-          placeholder="Email Address"
-          className=" mb-4 block border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2"
-        />
+        <div className="flex mb-4 border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
 
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password"
-          placeholder="Password"
-          className=" mb-4 block border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2"
-        />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            placeholder="Email Address"
+            className=" ml-4 w-full"
+            style={{ outline: "none" }}
+          />
+        </div>
+
+        <div className=" flex flex-row mb-4  border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+            />
+          </svg>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="Password"
+            className="ml-4 w-full"
+            style={{ outline: "none" }}
+          />
+        </div>
+
         <button
           type="submit"
           className=" bg-[#0575E6] text-white text-center w-full rounded-[30px] py-4 mb-1 hover:bg-[#35649b] transition duration-300"
--- src/components/landing-page/RegisterForm.tsx
+++ src/components/landing-page/RegisterForm.tsx
@@ -29,46 +29,115 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmit, error }) => {
       </div>
       <form onSubmit={handleSubmit} className=" text-md">
         <div className="flex space-x-2">
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            id="firstName"
-            placeholder="First Name"
-            required
-            className="mb-4 block border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-1"
-          />
+          <div className="flex flex-row mb-4 border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              id="firstName"
+              placeholder="First Name"
+              required
+              className="ml-4 w-full"
+              style={{ outline: "none" }}
+            />
+          </div>
+
+          <div className="flex flex-row mb-4 border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              id="lastName"
+              placeholder="Last Name"
+              required
+              className="ml-4 w-full"
+              style={{ outline: "none" }}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-row mb-4 border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
 
           <input
             type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            id="lastName"
-            placeholder="Last Name"
-            required
-            className="mb-4 block border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-1"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            placeholder="Email Address"
+            className=" ml-4 w-full"
+            style={{ outline: "none" }}
           />
         </div>
 
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          id="email"
-          placeholder="Email"
-          required
-          className="mb-4 block border border-gray-300 rounded-[30px] w-full  py-4 pl-6 pr-1"
-        />
-
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password"
-          placeholder="Password"
-          required
-          className="mb-4 block border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-1"
-        />
+        <div className=" flex flex-row mb-4  border border-gray-300 rounded-[30px] w-full py-4 pl-6 pr-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+            />
+          </svg>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="Password"
+            className="ml-4 w-full"
+            style={{ outline: "none" }}
+          />
+        </div>
 
         <button
           type="submit"
